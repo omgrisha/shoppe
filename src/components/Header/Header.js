182b0739729c8e5fa0875037c3ff7cc8c7affbd3
Grisha Korobeynikov <tywerqube@gmail.com>
2024-03-28 20:02:17 +0400

    Create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import StyledHeader from './Header.styled';
+
+const Header = () => {
+  return (
+    <StyledHeader>
+
+    </StyledHeader>
+  )
+};
+
+export default Header;
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled`
+
+`;
+
+export default StyledHeader;
