0f4105adfad0fcbeb95bb91c5a9f6574a9bad07a
Grisha Korobeynikov <tywerqube@gmail.com>
2024-03-28 19:43:22 +0400

    Add fonts

--- src/assets/globalStyles.js
+++ src/assets/globalStyles.js
@@ -1,7 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'DM Sans';
+    font-weight: 400;
+    src: url(./fonts/dm-sans-latin-400-normal.woff2);
+  }
 
+  @font-face {
+    font-family: 'DM Sans';
+    font-weight: 500;
+    src: url(./fonts/dm-sans-latin-500-normal.woff2);
+  }
+
+  @font-face {
+    font-family: 'DM Sans';
+    font-weight: 700;
+    src: url(./fonts/dm-sans-latin-700-normal.woff2);
+  }
+
+  @font-face {
+    font-family: 'Allerta Stencil';
+    font-weight: 400;
+    src: url(./fonts/allerta-stencil-latin-400-normal.woff2);
+  }
 `;
 
 export default GlobalStyle;
