9f6bb29a6c75faba0c8224400c41f63129c31d25
Grisha Korobeynikov <tywerqube@gmail.com>
2024-03-28 20:27:12 +0400

    Add Header's markup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,30 @@
 import StyledHeader from './Header.styled';
 
+import './search.svg';
+import './cart.svg';
+import './profile.svg';
+
 const Header = () => {
   return (
     <StyledHeader>
-
+      <div className="header__left-part">
+        <div class="header__logo"><span class="header__logo-first-letter">S</span>hoppe</div>
+      </div>
+      <div className="header__right-part">
+        <nav>
+          <ul>
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Our Story</a></li>
+          </ul>
+        </nav>
+        <div className="header__separator" />
+        <div className="header__icons">
+          <div><img src="./search.svg" alt="Search" /></div>
+          <div><img src="./cart.svg" alt="Cart" /></div>
+          <div><img src="./profile.svg" alt="Profile" /></div>
+        </div>
+      </div>
     </StyledHeader>
   )
 };
