4c0e1e4807086729d6e4c389946daa7a17cfc8ab
Grisha Korobeynikov <tywerqube@gmail.com>
2024-03-30 18:25:37 +0400

    Add vars.js and light theme colors in there

---
+++ src/assets/styles/vars.js
@@ -0,0 +1,7 @@
+export const ERRORS_COLOR_LIGHT = "#D82700";
+export const ACCENT_COLOR_LIGHT = "#A18A68";
+export const BLACK_COLOR_LIGHT = "#000000";
+export const DARK_GRAY_COLOR_LIGHT = "#707070";
+export const GRAY_COLOR_LIGHT = "#D8D8D8";
+export const LIGHT_GRAY_COLOR_LIGHT = "#EFEFEF";
+export const WHITE_COLOR_LIGHT = "#FFFFFF";
