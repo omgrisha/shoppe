362194c313566203648c21b87ee98cac2a8e8b86
Grisha Korobeynikov <tywerqube@gmail.com>
2024-04-01 16:14:06 +0400

    Add padding to Home, put Header into Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
-const Home = () => {
+import styled from 'styled-components';
 
+import Header from '../components/Header/Header';
+
+const StyledHome = styled.div`
+  padding: 64px 0 90px 0;
+`;
+
+const Home = () => {
+  return (
+    <StyledHome>
+      <Header />
+    </StyledHome>
+  );
 };
 
 export default Home;
