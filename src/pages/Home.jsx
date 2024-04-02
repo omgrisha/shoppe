a8908512b2515881c7ca57827a5b2b1ba0774a09
Grisha Korobeynikov <tywerqube@gmail.com>
2024-04-02 20:56:42 +0400

    feat(home): add footer component to markup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 const StyledHome = styled.div`
   padding: 64px 0 90px 0;
@@ -10,6 +11,7 @@ const Home = () => {
   return (
     <StyledHome>
       <Header />
+      <Footer />
     </StyledHome>
   );
 };
