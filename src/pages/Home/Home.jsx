ad0e1980ef6f1693b9c82d63edc85c864e9376e1
Grisha Korobeynikov <tywerqube@gmail.com>
2024-04-05 22:14:27 +0400

    feat(Home): add 'Shop the Latest' section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,35 @@
-import { StyledHome } from './Home.styled';
+import { StyledHome, Latest, Heading, ViewAll, Products } from './Home.styled';
 
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
+import { CardItem } from '../../components/CardItem';
+
+import { products } from '../../data/products';
 
 export const Home = () => {
   return (
     <StyledHome>
       <Header />
+      <div className="container">
+        <Latest>
+          <header className='flex'>
+            <Heading className='heading-1'>Shop The Latest</Heading>
+            <ViewAll className='heading-4'><a href="#">View all</a></ViewAll>
+          </header>
+          <Products>
+            {products.map((product, index) => (
+              <CardItem
+                key={index}
+                href='#'
+                image={product.image}
+                title={product.title}
+                price={product.price}
+              />
+            ))}
+          </Products>
+        </Latest>
+      </div>
+
       <Footer />
     </StyledHome>
   );
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
+import { BLACK_COLOR_LIGHT, ACCENT_COLOR_LIGHT } from '../../assets/styles/vars';
+
 export const StyledHome = styled.div`
   padding: 64px 0 90px 0;
+`;
+
+export const Latest = styled.section`
+  margin-top: 4rem;
+`;
+
+export const Heading = styled.h2`
+  color: ${BLACK_COLOR_LIGHT};
+`;
+
+export const ViewAll = styled.h3`
+  a {
+    color: ${ACCENT_COLOR_LIGHT};
+    text-decoration: none;
+  }
+`
+
+export const Products = styled.div`
+  margin-top: 2.4375rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 23.6rem);
+  justify-content: space-between;
+  gap: 5.375rem 0;
 `;
