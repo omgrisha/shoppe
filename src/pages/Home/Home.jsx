b62de71534fe8b7209fef571d227a86de4a5081e
Grisha Korobeynikov <tywerqube@gmail.com>
2024-04-05 18:17:12 +0400

    refactor(Home): move styling into a separate file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,7 @@
-import styled from 'styled-components';
+import { StyledHome } from './Home.styled';
 
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
-
-const StyledHome = styled.div`
-  padding: 64px 0 90px 0;
-`;
 
 export const Home = () => {
   return (
---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledHome = styled.div`
+  padding: 64px 0 90px 0;
+`;
