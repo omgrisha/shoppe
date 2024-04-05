ebefdf2e95667ee2a4a30f2b518b2bd9c2791f97
Grisha Korobeynikov <tywerqube@gmail.com>
2024-04-05 20:18:01 +0400

    fix(Home): fix wrong import paths caused by moving the file into Home folder

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { StyledHome } from './Home.styled';
 
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
 export const Home = () => {
   return (
